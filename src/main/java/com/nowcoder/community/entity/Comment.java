a71195c8958231185e4c66090d1e4f066436fa3c
linchuang <853540835@qq.com>
2020-07-19 23:44:54 +0800

    2020/7/19提交

---
+++ src/main/java/com/nowcoder/community/entity/Comment.java
@@ -0,0 +1,93 @@
+package com.nowcoder.community.entity;
+
+import java.util.Date;
+
+public class Comment {
+
+    private int id;
+    private int userId; //发起人
+    private int entityType;//评论的类型：1表示帖子，2表示评论
+    private int entityId;//指回复评论的那个Id：比如帖子的Id
+    private int targetId;//指对某个人的评论进行回复，指人的Id
+    private String content;
+    private int status;
+    private Date createTime;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getEntityType() {
+        return entityType;
+    }
+
+    public void setEntityType(int entityType) {
+        this.entityType = entityType;
+    }
+
+    public int getEntityId() {
+        return entityId;
+    }
+
+    public void setEntityId(int entityId) {
+        this.entityId = entityId;
+    }
+
+    public int getTargetId() {
+        return targetId;
+    }
+
+    public void setTargetId(int targetId) {
+        this.targetId = targetId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", entityType=" + entityType +
+                ", entityId=" + entityId +
+                ", targetId=" + targetId +
+                ", content='" + content + '\'' +
+                ", status=" + status +
+                ", createTime=" + createTime +
+                '}';
+    }
+}
